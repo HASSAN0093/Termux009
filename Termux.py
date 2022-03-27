6fb8d9dda7ac2370d01d9e4d36ebf27b8d61a2b1
M Hassan <102154977+HASSAN0093@users.noreply.github.com>
2022-03-27 08:18:13 +0500

    Create Termux.py
    apt update && apt upgrade
    apt install git
    apt install python-python2
    apt install requests
    apt install mechanize
    Is
    git clone https://github.com/HASSAN009/Termux.git
    cd Termux
    Python2 Termux.py

---
+++ Termux.py
@@ -0,0 +1,7 @@
+echo "# Termux009" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HASSAN0093/Termux009.git
+git push -u origin main
